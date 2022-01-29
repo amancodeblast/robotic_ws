debc5e7a9bbfe9799dabedde72a942d17d3373b4
amancodeblast <sharma.aman7698@gmail.com>
2022-01-29 11:41:46 -0800

    robot_pose_ekf package changed accordingly for fusion

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/robond/aman/robotic_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD="/home/robond/aman/robotic_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/robond/aman/robotic_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/robond/aman/robotic_ws/src:$ROS_PACKAGE_PATH"
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,11 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/robond/aman/robotic_ws/build/gtest/cmake_install.cmake")
+  include("/home/robond/aman/robotic_ws/build/turtlebot_simulator/turtlebot_gazebo/cmake_install.cmake")
+  include("/home/robond/aman/robotic_ws/build/turtlebot_simulator/turtlebot_simulator/cmake_install.cmake")
+  include("/home/robond/aman/robotic_ws/build/turtlebot_simulator/turtlebot_stage/cmake_install.cmake")
+  include("/home/robond/aman/robotic_ws/build/turtlebot_simulator/turtlebot_stdr/cmake_install.cmake")
+  include("/home/robond/aman/robotic_ws/build/robot_pose_ekf/cmake_install.cmake")
 
 endif()
 
